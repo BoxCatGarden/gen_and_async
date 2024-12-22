d27e85ec861f3dd1bef0245d0ff2e353ca1d0ec5
boxcatgarden <1004149525@qq.com>
2024-12-23 03:11:38 +0800

    test

--- test_native/async_gen/test_async_gen_lv.js
+++ test_native/async_gen/test_async_gen_lv.js
@@ -3,76 +3,124 @@ async function* gen() {
     return 6;
 }
 
-let b;
+async function a() {
+    let b;
+    let q;
 
-b = gen();
-b.next().then().then(v => console.log('next=2'));
-b.return(1).then(v => console.log('return=1'));
+    console.log('=================');
 
-b = gen();
-b.next().then().then(v => console.log('next=2 - 1'));
-b.next().then().then(v => console.log('next=2 - 2'));
-b.return(1).then(v => console.log('return=1'));
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2')));
+    q.push(b.return(1).then(v => console.log('return=1')));
 
-b = gen();
-b.return(1).then().then(v => console.log('return=2'));
-b.next().then(v => console.log('next=1'));
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.next().then().then(v => console.log('next=2 - 1'));
-b.next().then().then(v => console.log('next=2 - 2'));
-b.return(1).then().then(v => console.log('return=2'));
-b.return(1).then(v => console.log('return=1'));
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2 - 1')));
+    q.push(b.next().then().then(v => console.log('next=2 - 2')));
+    q.push(b.return(1).then(v => console.log('return=1')));
+    
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.return(1).then().then(v => console.log('return=2'));
-b.return(1).then(v => console.log('return=1'));
+    b = gen(); q = [];
+    q.push(b.return(1).then().then(v => console.log('return=2')));
+    q.push(b.next().then(v => console.log('next=1')));
 
-b = gen();
-b.next().then().then(v => console.log('next=2'));
-b.throw(1).catch(v => console.log('throw=1'));
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.next().then().then(v => console.log('next=2 - 1'));
-b.next().then().then(v => console.log('next=2 - 2'));
-b.throw(1).catch(v => console.log('throw=1'));
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2 - 1')));
+    q.push(b.next().then().then(v => console.log('next=2 - 2')));
+    q.push(b.return(1).then().then(v => console.log('return=2')));
+    q.push(b.return(1).then(v => console.log('return=1')));
 
-b = gen();
-b.throw(1).then().catch(v => console.log('throw=2'));
-b.next().then(v => console.log('next=1'));
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.next().then().then(v => console.log('next=2 - 1'));
-b.next().then().then(v => console.log('next=2 - 2'));
-b.throw(1).then().catch(v => console.log('throw=2'));
-b.throw(1).catch(v => console.log('throw=1'));
+    b = gen(); q = [];
+    q.push(b.return(1).then().then(v => console.log('return=2')));
+    q.push(b.return(1).then(v => console.log('return=1')));
 
-b = gen();
-b.throw(1).then().catch(v => console.log('throw=2'));
-b.throw(1).catch(v => console.log('throw=1'));
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.return(1).then().then(v => console.log('return=2'));
-b.throw(1).catch(v => console.log('throw=1'));
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2')));
+    q.push(b.throw(1).catch(v => console.log('throw=1')));
 
-b = gen();
-b.throw(1).then().catch(v => console.log('throw=2'));
-b.return(1).then(v => console.log('return=1'));
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.next().then().then(v => console.log('next=2'));
-b.next().then(v => console.log('next=1'));
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2 - 1')));
+    q.push(b.next().then().then(v => console.log('next=2 - 2')));
+    q.push(b.throw(1).catch(v => console.log('throw=1')));
 
-b = gen();
-b.next().then().then(v => console.log('next=2 - 1'));
-b.next().then().then(v => console.log('next=2 - 2'));
-b.next().then(v => console.log('next=1'));
+    await Promise.all(q);
+    console.log('=================');
 
-b = gen();
-b.next().then().then(v => console.log('next=2 - 1'));
-b.next().then().then(v => console.log('next=2 - 2'));
-b.next().then().then(v => console.log('next=2 - 3'));
-b.next().then(v => console.log('next=1'));
+    b = gen(); q = [];
+    q.push(b.throw(1).then().catch(v => console.log('throw=2')));
+    q.push(b.next().then(v => console.log('next=1')));
 
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2 - 1')));
+    q.push(b.next().then().then(v => console.log('next=2 - 2')));
+    q.push(b.throw(1).then().catch(v => console.log('throw=2')));
+    q.push(b.throw(1).catch(v => console.log('throw=1')));
+
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.throw(1).then().catch(v => console.log('throw=2')));
+    q.push(b.throw(1).catch(v => console.log('throw=1')));
+
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.return(1).then().then(v => console.log('return=2')));
+    q.push(b.throw(1).catch(v => console.log('throw=1')));
+
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.throw(1).then().catch(v => console.log('throw=2')));
+    q.push(b.return(1).then(v => console.log('return=1')));
+
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2')));
+    q.push(b.next().then(v => console.log('next=1')));
+
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2 - 1')));
+    q.push(b.next().then().then(v => console.log('next=2 - 2')));
+    q.push(b.next().then(v => console.log('next=1')));
+
+    await Promise.all(q);
+    console.log('=================');
+
+    b = gen(); q = [];
+    q.push(b.next().then().then(v => console.log('next=2 - 1')));
+    q.push(b.next().then().then(v => console.log('next=2 - 2')));
+    q.push(b.next().then().then(v => console.log('next=2 - 3')));
+    q.push(b.next().then(v => console.log('next=1')));
+}
+
+a();
 
 
