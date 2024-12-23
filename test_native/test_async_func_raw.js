9d45ae5be4f92a15da44dabb8964e2cc071b9b6b
boxcatgarden <1004149525@qq.com>
2024-12-24 01:54:29 +0800

    test

--- test_native/test_async_func_raw.js
+++ test_native/test_async_func_raw.js
@@ -7,8 +7,13 @@ async function a(n, v) {
     }, 0);
     v = await (v + 1);
     console.log(`value=${v}`);
+    return 22;
 }
 
-a(1, 1);
+Promise.resolve()
+    .then().then().then(v => console.log('then=3'));
+Promise.resolve()
+    .then().then().then().then(v => console.log('then=4'));
+a(1, 1).then(v => console.log('return =', v));
 a(2, 3);
 console.log('after call');
