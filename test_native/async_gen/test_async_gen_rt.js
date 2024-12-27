9cd74fa7514bc865f57e92db63373fdd8655c55a
boxcatgarden <1004149525@qq.com>
2024-12-27 12:49:40 +0800

    test

---
+++ test_native/async_gen/test_async_gen_rt.js
@@ -0,0 +1,160 @@
+async function* gen() {
+    yield 11;
+    return 22;
+}
+
+async function a(ret) {
+    const _return = ret ? 'return' : 'throw';
+    const _then = ret ? 'then' : 'catch';
+    let b;
+    let idx = 0;
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b.next().then(v => console.log('value =', v));
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    b.next().then(v => console.log('value =', v));
+    b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b.next().then(v => console.log('value =', v));
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+
+    console.log(`$${++idx}: ==============`);
+    b = gen();
+    await b[_return](idx)[_then](v => console.log(`${_return} =`, v));
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+    await b.next().then(v => console.log('value =', v));
+}
+
+async function run() {
+    console.log('return: =======================');
+    await a(true);
+    console.log('throw: ========================');
+    await a(false);
+}
+
+run();
