cd286070e81d6c505b746dc279b92ddee3841a5f
boxcatgarden <1004149525@qq.com>
2025-01-03 07:47:14 +0800

    g2ag

--- nodejs/gen_to_async_gen.js
+++ nodejs/gen_to_async_gen.js
@@ -1,5 +1,7 @@
 /**
  * Wrap `@value` as a yielded value.
+ * @param value - The value to be wrapped.
+ * @return - Wrapped value.
  * @see _yield
  * */
 const Yielded = (value) => ({
@@ -9,6 +11,8 @@ const Yielded = (value) => ({
 
 /**
  * Wrap `@value` as an awaited value.
+ * @param value - The value to be wrapped.
+ * @return - Wrapped value.
  * @see _await
  * */
 const Awaited = (value) => ({
@@ -33,13 +37,35 @@ const _await = (expResult) => {
 /**
  * To yield a value, use it like the following:
  * ```
- * let val = _yield(yield Yielded(value));
+ * let nextInput = _yield(yield Yielded(value));
  * ```
  * @see Yielded
  * */
 const _yield = _await;
 
-
 class AsyncGenerator {
-
+    constructor(genFunc, args) {
+    }
 }
+
+/**
+ * Wrap a generator function as an async generator function.
+ * @param genFunc - The generator function.
+ * @return - The async generator function.
+ * @see _await
+ * @see _yield
+ * @see Awaited
+ * @see Yielded
+ * */
+const __async_star = (genFunc) => {
+    return (...args) => new AsyncGenerator(genFunc, args);
+};
+
+
+module.exports = {
+    Yielded,
+    Awaited,
+    _yield,
+    _await,
+    __async_star,
+};
