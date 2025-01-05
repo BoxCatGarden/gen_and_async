728bcac790d0087d66d7ec973b407966efac687d
boxcatgarden <1004149525@qq.com>
2025-01-05 08:14:23 +0800

    test

--- test/scripts/Run-All.ps1
+++ test/scripts/Run-All.ps1
@@ -2,16 +2,19 @@ $Local = Split-Path -Parent $PSCommandPath
 $Project = "$Local\..\.."
 $Output = "$Local\..\output"
 
+'' > "$Output\async_func.txt"
 & "$Local\compare.ps1" `
 "$Project\nodejs\test\async_func" `
 "$Project\test_native\async_func" `
 "$Output\async_func.txt"
 
+'' > "$Output\async_gen.txt"
 & "$Local\compare.ps1" `
 "$Project\nodejs\test\async_gen" `
 "$Project\test_native\async_gen" `
 "$Output\async_gen.txt"
 
+'' > "$Output\async_gen_gen.txt"
 & "$Local\compare.ps1" `
 "$Project\nodejs\test\async_gen_gen" `
 "$Project\test_native\async_gen" `
